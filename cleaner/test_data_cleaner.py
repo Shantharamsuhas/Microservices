c1f421f9d099f5c6f07febba11d6d055af8df151
Priyanka Byahatti <priyankabyahatti@gmail.com>
2020-12-16 18:43:42 +0100

    updated data_cleaner and added test unit scripts for the same

---
+++ cleaner/test_data_cleaner.py
@@ -0,0 +1,26 @@
+import pytest
+from cleaner.data_cleaner import database_connection, create_processed_movie_rating_table, \
+    load_processed_movie_rating_table
+
+
+# Test if db connected
+def test_db_connection():
+    conn = database_connection()
+    assert conn is not None, 'Test passed connected to database successfully'
+
+
+# Test if rating table is created
+def test_rating_table_exists():
+    is_table = create_processed_movie_rating_table()
+    assert len(is_table) != 0, 'Test passed rating table exists'
+
+
+# Test if rating table has data
+def test_data_rating_table():
+    has_data = load_processed_movie_rating_table()
+    assert len(has_data) != 0, 'Test passed rating table has data'
+
+
+# run the file
+if __name__ == '__main__':
+    pytest.main()
