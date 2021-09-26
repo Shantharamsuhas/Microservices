b22f48bed9771132e5ec024fdfbe8b36744aa538
Nikhil <56631042+iamitaliya@users.noreply.github.com>
2021-09-26 18:09:56 +0200

    Update server.test.js

--- visualiser/server.test.js
+++ visualiser/server.test.js
@@ -2,7 +2,7 @@ const toRows = require('./server');
 
 const data = [['a', 2, 'c'], ['a', 2, 'c'], ['a', 2, 'c']];
 const rowCount = 3;
-const result = [['a', 'a', 'a'], [2, 2, 2]]
+const result = [['a', 'a', 'a'], ["2.0", "2.0", "2.0"]]
 
 test('Testing toRows method', () => {
     expect(toRows(data, rowCount)).toStrictEqual(result);
