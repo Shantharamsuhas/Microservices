9284651f8ed7b0e124cd420c342672f8b2c7206d
Priyanka Byahatti <85172710+priyanka-byahatti@users.noreply.github.com>
2021-10-21 21:38:50 +0200

    - js scraper plot synopsis

--- visualiser/server.js
+++ visualiser/server.js
@@ -1,4 +1,7 @@
 var express = require('express');
+const axios = require("axios");
+const cheerio = require("cheerio");
+const pretty = require("pretty");
 var app = express();
 var path = require('path');
 var fs = require('fs')
@@ -264,3 +267,40 @@ function toRows(data, rowCount, hastconst = false) {
 //     })
 // }
 module.exports = toRows;
+
+
+// SCRAPER 
+const fetchTitles = async () => {
+    try {
+    // get current page url
+     let current_page = 'http://localhost:8080/details_page/tt0068646' 
+    //  let current_page = window.location.href
+    // extract tconst id
+     let tconst = current_page.match(/(?<!\w)tt\w+/g);
+     const response = await        
+     axios.get('https://www.imdb.com/title/'+tconst+'/plotsummary?ref_=tt_stry_pl#synopsis');
+       
+        const html = response.data;
+  
+     const $ = cheerio.load(html);
+    //  console.log(pretty($.html()));
+     const synopsys = [];
+   
+     $('#plot-synopsis-content').each((_idx, el) => {
+      const title = $(el).text()
+    //   console.log(title)
+      synopsys.push(title)
+     });
+   
+     return synopsys;
+    } catch (error) {
+     throw error;
+    }
+   };
+  
+    
+   async function call_scraper() {
+    let abc = await fetchTitles();
+    // console.log(abc);
+    return abc
+  }
