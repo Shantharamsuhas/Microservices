e1c17eefcbaf62b3a69229b34aa5466c26863932
Priyanka Byahatti <85172710+priyanka-byahatti@users.noreply.github.com>
2021-10-23 21:31:01 +0200

    - function api  to fetch tags

--- visualiser/js/scrape_synopsis.js
+++ visualiser/js/scrape_synopsis.js
@@ -15,3 +15,22 @@ function fetchTitles(){
       }
       });
   };
+
+
+
+  function fetchTags(){
+    data = {data : document.getElementById('synpsis_text_area').value}
+    fetch("http://localhost:3311/get_synopsis", {
+      method: "POST", headers: {'Content-Type': 'application/json'}, 
+      body: JSON.stringify(data)
+      }).then(response => response.json()).then(res => {
+      console.log("Requested completed: ", res.res, res.tags);
+      if(res.res == "success"){
+      document.getElementById('h1').value = res.tags.trim()
+      }
+      else {
+      document.getElementById('h1').value = "Error recieving data"
+      console.log("Error recieving data")
+      }      
+    });
+  };
