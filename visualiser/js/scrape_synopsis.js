b63b502dd5d909c3e0c9fc243092cac3bdc9245b
Nikhilkumar Italiya <nikhilkumar.italiya@st.ovgu.de>
2021-10-24 12:07:04 +0200

    added recommendations

--- visualiser/js/scrape_synopsis.js
+++ visualiser/js/scrape_synopsis.js
@@ -2,11 +2,12 @@ function fetchTitles(){
   var url = window.location.href
   movie_id = url.split("/").at(-1)
   data = {tconst : movie_id}
+  fetchRecommendations(movie_id)
   fetch("http://localhost:8080/get_titles", {
-      method: "POST",headers: {'Content-Type': 'application/json'}, 
+      method: "POST",  headers: {'Content-Type': 'application/json'}, 
       body: JSON.stringify(data)
       }).then(response => response.json()).then(res => {
-      console.log("Request complete! response:", res.res, res.synopsys);
+      console.log("Request complete! response:", res.res);
       if(res.res == "success"){
       document.getElementById('synpsis_text_area').value = res.synopsys.trim()
       }else{
@@ -16,21 +17,35 @@ function fetchTitles(){
       });
   };
 
-
-
   function fetchTags(){
     data = {data : document.getElementById('synpsis_text_area').value}
-    fetch("http://localhost:3311/get_synopsis", {
+    fetch("http://localhost:3311/get-synopsis", {
       method: "POST", headers: {'Content-Type': 'application/json'}, 
       body: JSON.stringify(data)
       }).then(response => response.json()).then(res => {
       console.log("Requested completed: ", res.res, res.tags);
       if(res.res == "success"){
-      document.getElementById('h1').value = res.tags.trim()
+        document.getElementById('h1').innerHTML = res.tags.trim()
       }
       else {
-      document.getElementById('h1').value = "Error recieving data"
-      console.log("Error recieving data")
+        document.getElementById('h1').innerHTML = "Error recieving data"
+        console.log("Error recieving data")
       }      
     });
+  };
+
+function fetchRecommendations(tconst){
+  fetch("http://localhost:2211/get-recommendation/id/" + tconst).then(response => response.json()).then(res => {
+      console.log("Request complete! response:", res.res, res.movies);
+      if(res.res == "success"){
+        if(res.movies != "failed"){
+          document.getElementById('p2').innerHTML = res.movies.trim()
+        }else{
+          document.getElementById('p2').innerHTML = "No recommendations found for this movie"
+        }
+      }else{
+      document.getElementById('p2').innerHTML = "Error recieving data"
+      console.log("Error recieving data")
+      }
+      });
   };
